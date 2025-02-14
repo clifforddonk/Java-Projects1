fcdace2a2c43fcd481563c5601969c0683787aae
clifforddonk <clifforddonk@gmail.com>
2025-02-14 20:55:32 +0000

    new changes

--- src/com/Clifford/Main.java
+++ src/com/Clifford/Main.java
@@ -13,6 +13,18 @@ public class Main {
 
         System.out.println("Enter second number");
         b =scanner.nextInt();  // Directly reading another integer
+
+        String str,str1;
+        System.out.println("Enter two words:");
+        str=scanner.next();
+        str1=scanner.next();
+
+        if(str.equalsIgnoreCase((str1)))
+            System.out.println(str + " is equal to "+ str1);
+        else
+            System.out.println(str + " is not equal to "+ str1);
+
+
 //Addition
         result = a+b;
         System.out.println("After adding:" + a +"+"+b +"=" +result);
