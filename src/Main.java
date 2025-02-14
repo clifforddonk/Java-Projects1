8a6d005eb51a6079e463db55b47a2c6d52c90b6a
clifforddonk <clifforddonk@gmail.com>
2025-02-14 18:12:26 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+   Scanner scanner=new Scanner(System.in);
+    int a=0,b=0;
+
+}
